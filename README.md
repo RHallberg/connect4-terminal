a8d8da367e491f617d47f9fc7a16557e1e2be582
RHallberg <hallbergrickard@hotmail.com>
2026-01-15 18:09:35 +0100

    Added win check

---
+++ README.md
@@ -0,0 +1,6 @@
+To compile on linux:
+```bash
+gcc connect4.c -o connect4 -Wall -Werror
+```
+
+![preview](preview.png "preview")
--- connect4.c
+++ connect4.c
@@ -3,13 +3,23 @@
 #define TB_IMPL
 #include "termbox2.h"
 
+#define BOARD_W 7
+#define BOARD_H 6
+
+#define PLAYER1 1
+#define PLAYER2 2
+
+#define WIN 1
 
 typedef struct {
     int x;
     int y;
 } Point;
 
-void dropDisc(int pos, int player, int (*grid)[6]);
+int dropDisc(int pos, int player, int (*grid)[6]);
+
+int countDiscs(int (*grid)[6], int x, int y, int player, int dx, int dy);
+int checkWin(int (*grid)[6], int x, int y, int player);
 
 int main()
 {
@@ -21,10 +31,12 @@ int main()
   }
 
   const int colors[3] = {TB_WHITE, TB_RED, TB_YELLOW};
-  int player = 1;
-  int grid [7][6];
-  for (int i = 0; i < 7; i++) {
-    for (int j = 0; j < 6; j++) {
+  const int textcolors[3] = {0, TB_WHITE, TB_BLACK};
+  int player = PLAYER1;
+  int gameover = 0;
+  int grid [BOARD_W][BOARD_H];
+  for (int i = 0; i < BOARD_W; i++) {
+    for (int j = 0; j < BOARD_H; j++) {
       grid[i][j] = 0;
     }
   }
@@ -34,16 +46,23 @@ int main()
     tb_peek_event(&e, 10);
     if(e.ch == 'q')break;
     if(e.ch == 'r'){
-      for (int i = 0; i < 7; i++) {
-        for (int j = 0; j < 6; j++) {
+      gameover = 0;
+      player = PLAYER1;
+      for (int i = 0; i < BOARD_W; i++) {
+        for (int j = 0; j < BOARD_H; j++) {
           grid[i][j] = 0;
         }
       }
     }
 
-    if(isdigit(e.ch) && e.ch - '0' <= 7){
-      dropDisc(e.ch - '0', player, grid);
-      player = player == 1 ? 2 : 1;
+    if(isdigit(e.ch) && e.ch - '0' <= BOARD_W && !gameover){
+      int result = dropDisc(e.ch - '0', player, grid);
+      if(result == WIN){
+        gameover = 1;
+      }
+      else {
+        player = player == PLAYER1 ? PLAYER2 : PLAYER1;
+      }
     }
 
     Point pos ={0,0};
@@ -52,19 +71,26 @@ int main()
     // Print the grid
     tb_print(pos.x, pos.y, TB_WHITE, TB_BLUE, "|1 2 3 4 5 6 7|");
     pos.y++;
-    for(int i = 0; i < 6; i++){
+    for(int i = 0; i < BOARD_H; i++){
       tb_print(pos.x, pos.y, TB_WHITE, TB_BLUE, "|             |");
       pos.y++;
     }
     tb_print(pos.x, pos.y, TB_WHITE, TB_BLUE, "|1 2 3 4 5 6 7|");
 
     // Print the discs
-    for (int i = 0; i < 7; i++) {
-      for (int j = 0; j < 6; j++) {
+    for (int i = 0; i < BOARD_W; i++) {
+      for (int j = 0; j < BOARD_H; j++) {
         int color = colors[grid[i][j]];
         int offset = i + 1;
         tb_print(i+offset,j+1, color, TB_BLUE, "⬤");
       }
+    }
+
+    if(gameover){
+      char winstring[25];
+      sprintf(winstring, "PLAYER %d WINS!!", player);
+      tb_print(pos.x, pos.y+1, textcolors[player], colors[player], winstring);
+      tb_print(pos.x, pos.y+2, TB_BLACK, TB_DEFAULT, "Press 'r' to reset");
     }
 
     tb_present();
@@ -74,13 +100,46 @@ int main()
   return 0;
 }
 
-void dropDisc(int pos, int player, int (*grid)[6]){
+int dropDisc(int pos, int player, int (*grid)[BOARD_H]){
   pos--;
   Point location = {pos,0};
-  while(grid[location.x][location.y] == 0 && location.x < 7 && location.x >= 0 && location.y < 6){
+  while(grid[location.x][location.y] == 0 && location.x < BOARD_W && location.x >= 0 && location.y < BOARD_H){
     location.y++;
   }
   location.y--;
-  if(location.y < 0) return;
+  if(location.y < 0) return 0;
   grid[location.x][location.y] = player;
+  // return 0;
+  return checkWin(grid, location.x, location.y, player);
+}
+
+int countDiscs(int (*grid)[6], int x, int y, int player, int dx, int dy){
+
+  int nbr = 0;
+  while(x >= 0 && y >= 0 && x < BOARD_W && y < BOARD_H && grid[x][y] == player){
+    nbr++;
+    x+= dx; y+= dy;
+  }
+  return nbr;
+}
+
+int checkWin(int (*grid)[6], int x, int y, int player){
+
+  // Vertical
+  if((countDiscs(grid, x, y+1, player, 0, 1) + 1) == 4) 
+    return 1;
+
+  // Horizontal
+  if((countDiscs(grid, x+1, y, player, 1, 0) + 1 + countDiscs(grid, x-1, y, player, -1, 0) ) == 4)
+    return 1;
+
+  // Diagonal right
+  if((countDiscs(grid,x+1,y-1, player, 1, -1) + 1 + countDiscs(grid, x-1, y+1, player, -1, 1)) == 4)
+    return 1;
+
+  // Diagonal left
+  if((countDiscs(grid,x-1,y-1, player, -1, -1) + 1 + countDiscs(grid, x+1, y+1, player, 1, 1)) == 4)
+    return 1;
+
+  return 0;
 }
